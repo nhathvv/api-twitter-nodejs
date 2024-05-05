3519b62fa80efb0df174dd54bdb24cca25debfc0
Nhathv21 <Nhathv.21it@gmail.com>
2024-05-05 15:14:21 +0700

    perf: init folder for upload

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import userRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import defaultErrorHandler from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 // Connect to MongoDB
 databaseService.connect()
 const app = express()
 const port = 4000
 app.use(express.json())
+// Init folder for upload
+initFolder()
 app.use('/users', userRouter)
 app.use('/medias', mediasRouter)
 // Defaut error handler
---
+++ src/utils/file.ts
@@ -0,0 +1,9 @@
+import fs from 'fs'
+import path from 'path'
+
+export const initFolder = () => {
+  const uploadFolderPath = path.resolve('uploads')
+  if (!fs.existsSync(uploadFolderPath)) {
+    fs.mkdirSync(uploadFolderPath, { recursive: true })
+  }
+}
