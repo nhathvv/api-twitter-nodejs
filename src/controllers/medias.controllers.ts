bca7a0d70bc388d55f4a97ce306253f05d8cebad
Nhathv21 <Nhathv.21it@gmail.com>
2024-05-06 14:34:25 +0700

    feat: filter upload image

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,17 +1,8 @@
 import { Request, Response } from 'express'
 import formidable from 'formidable'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 export const uploadSingleImageController = async (req: Request, res: Response) => {
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    keepExtensions: true,
-    maxFileSize: 3 * 1024 * 1024,
-    maxFiles: 1
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    return res.status(200).json({ message: 'Upload single image success' })
-  })
+  const data = await handleUploadSingleImage(req)
+  return res.status(200).json({ result: data })
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,9 +1,37 @@
+import { Request, Response } from 'express'
 import fs from 'fs'
 import path from 'path'
+import formidable from 'formidable'
 
 export const initFolder = () => {
   const uploadFolderPath = path.resolve('uploads')
   if (!fs.existsSync(uploadFolderPath)) {
     fs.mkdirSync(uploadFolderPath, { recursive: true })
   }
+}
+export const handleUploadSingleImage = (req: Request) => {
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFileSize: 3 * 1024 * 1024,
+    maxFiles: 1,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const isValid = name === 'image' && Boolean(mimetype?.includes('image'))
+      if (!isValid) {
+        form.emit('error' as any, new Error('Invalid file type') as any)
+      }
+      return isValid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!files.image) {
+        return reject(new Error('No image uploaded'))
+      }
+      return resolve(files)
+    })
+  })
 }
