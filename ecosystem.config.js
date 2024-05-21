936d09539ebb60d74c5496720a8b2ac9b74be40b
Nhathv21 <Nhathv.21it@gmail.com>
2024-05-21 14:07:57 +0700

    ci: add dockerfile and docker compose for app deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       name: 'twitter',
       script: 'node dist/index.js',
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: 'development'
       },
       env_production: {
         NODE_ENV: 'production'
